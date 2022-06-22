3e460cd691afe4278c96620db65661ed6de3d41f
m.semenov <rasmiy@razum.top>
2022-06-22 18:02:03 +0500

    Добавил кнопку и подписку

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,8 @@ export class AppComponent {
   constructor() {
     
   }
+
+  onButtonClick() {
+    console.log('buttonClicked');
+  }
 }
--- src/app/components/qt-table/qt-table.component.ts
+++ src/app/components/qt-table/qt-table.component.ts
@@ -1,17 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
-// import { tap } from "rxjs";
+import { takeUntil, tap } from 'rxjs/operators';
+import { DestroyService } from 'src/app/services/destroy.service';
 import { AbstractDataModel, Direction } from './abstract-data-model';
 import { EmptyDataModel } from './empty-data-model';
 
 @Component({
   selector: 'app-qt-table',
   templateUrl: './qt-table.component.html',
-  styleUrls: ['./qt-table.component.scss']
+  styleUrls: ['./qt-table.component.scss'],
+  providers: [
+    DestroyService
+  ]
 })
 export class QtTableComponent implements OnInit {
   @Input() model: AbstractDataModel = new EmptyDataModel();
 
-  constructor() {
+  constructor(
+    private readonly destroy$: DestroyService
+  ) {
   }
 
   ngOnInit(): void {
@@ -27,11 +33,12 @@ export class QtTableComponent implements OnInit {
 
   setModel(model: AbstractDataModel) {
     this.model = model;
-    // this.model.dataChanged.asObservable().pipe(
-    //   tap(event => {
-
-    //   })
-    // ).subscribe();
+    this.model.dataChanged.asObservable().pipe(
+      tap(event => {
+        console.log('dataChanged', event)
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe();
   }
 
   dataHeader(index: number) {
